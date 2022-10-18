73cebe9320d8352e613f76e7283c4a4c1c7eacc2
Giovanni-831 <114507294+Giovanni-831@users.noreply.github.com>
2022-10-17 21:00:51 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+* Author: Giovanni Lomeli <glomelim@ucsc.edu>
+* created: 17 September
+* License: Public Domain
+*/
+
+// Define Variables
+make = "honda";
+model = "Civic";
+color = "White";
+year = "2017";
+
+// calculate
+age = 2022 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
