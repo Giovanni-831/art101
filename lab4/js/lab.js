ef5570f1eb4f6dbd35e5ce661ffb6d7091b19cf9
Giovanni-831 <114507294+Giovanni-831@users.noreply.github.com>
2022-10-30 19:30:51 -0700

    update

---
+++ lab4/js/lab.js
@@ -0,0 +1,20 @@
+/*
+* Author: Giovanni Lomeli <glomelim@ucsc.edu>
+* created: October 30
+* lincense: public Domain
+*/
+
+// Define Variables
+
+
+
+
+
+// calcualte
+
+
+
+//output
+document.writeln
+document.writeln
+document.writeln
