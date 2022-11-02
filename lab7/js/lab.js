93627df7a5b9ae2895c0c5386fb6805db303fda0
Giovanni-831 <114507294+Giovanni-831@users.noreply.github.com>
2022-11-01 17:50:49 -0700

    Update lab.js

---
+++ lab7/js/lab.js
@@ -0,0 +1,25 @@
+/*
+* Author: Giovanni Lomeli <glomelim@ucsc.edu>
+* Author: Aakaash Naraannan <anaraya4@ucsc.edu
+* License: Public Domain
+*/
+
+function sortname() {
+  var userName = window.promt("please tell me your name:");
+  console.log("username =", userName);
+  //split string to array
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
+  // sort the nameArray
+  car name Arraysort = nameArray.sort();
+  console.log("nameSorted = ", nameArraySort);
+  // join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted = ", nameSorted);
+
+  return nameSorted;
+}
+
+// Output
+document.writeln("Hey, I fixed your name:",
+  sortName(), "</br>");
