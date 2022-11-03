35de6e3f133fe3244a6847289b957a5150fadd00
Giovanni-831 <114507294+Giovanni-831@users.noreply.github.com>
2022-11-02 21:34:20 -0700

    update

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,17 +1,16 @@
 /*
 * Author: Giovanni Lomeli <glomelim@ucsc.edu>
-* Author: Aakaash Nrayannan <anaraya4@ucsc.edu>
 *License: Public Domain
 */
 var outputEl = document.getElementByid("output");
 
 
 var new1El = document.createElement("p");
-new1El.innerHTML = "Hello, Our names are Gio and Aakaash.";
+new1El.innerHTML = "Hello, my name is Gio";
 new1El.appendChild(new1El);
 new1El.style.color='blue';
 
 var new2El = document.createelement("p");
-new2El.innerHTML = "Gio and Aakaash love pizza";
+new2El.innerHTML = "Giovanni love pizza";
 outputEl.appendChild(new2El);
 new2El.style.fontSize="20pt";
