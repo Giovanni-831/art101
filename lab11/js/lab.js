382ed6a87320ecc2f1fde3cd48561ff0e64f0a47
Giovanni-831 <114507294+Giovanni-831@users.noreply.github.com>
2022-11-09 22:01:23 -0800

    Update lab.js

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -9,9 +9,12 @@ k.fn.init {div#headshot}
   [ [Prototype]]: object (0)
 
   // new element on the page
-  k.fn.inity[button#my-button];
+k.fn.inity[button#my-button];
   0: button#my-button
   length: 1
   [[Prototype]]: Object(0)
 
-  
+
+Click(function(){
+  .append("<button>No, press me!");
+});
