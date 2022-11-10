acac6af5428910891a59f35fe219364ac11f736d
Giovanni-831 <114507294+Giovanni-831@users.noreply.github.com>
2022-11-09 21:56:50 -0800

    Update lab.js

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -7,4 +7,11 @@ k.fn.init {div#headshot}
   0: div# headshot
   length: 1
   [ [Prototype]]: object (0)
+
+  // new element on the page
+  k.fn.inity[button#my-button];
+  0: button#my-button
+  length: 1
+  [[Prototype]]: Object(0)
+
   
