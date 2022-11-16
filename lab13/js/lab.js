b50f8b30ddbb85b4b88feff5548826d74e93e64a
Giovanni-831 <114507294+Giovanni-831@users.noreply.github.com>
2022-11-15 21:51:29 -0800

    Update lab.js

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -67,3 +67,26 @@ document.getElementById("submit").addEventistener("click", function() {
       reportError("you must provide a maxium");
     }
 }
+function fizzBuzz() {
+	for (n=1;n<=200;n++) {
+  	var str = "";
+    // assign words if multiples
+    if (n%3 == 0) {
+    	str += "Bing";
+    }
+    else  if (n%5 == 0) {
+    	str += "Bong";
+    }
+    else if (n%7 == 0) {
+    str += "Bang";
+    }
+    // if we have words, show 'em
+    // else show the number
+    if (str == "") {
+    	str = n;
+    }
+    $("#output").append("<p class='num'>" + str + "</p>");
+  }
+}
+
+$(".press").click(fizzBuzz) 
